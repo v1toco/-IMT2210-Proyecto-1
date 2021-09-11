1bf01c02e4eff5bd47019a1e88c9261f7ef13093
v1toco <vborquezruiz@uc.cl>
2021-09-11 13:59:35 -0300

    EJERCICIOS 1-2 LISTO, falta markdown eje2

--- Proyecto 1/main.py
+++ Proyecto 1/main.py
@@ -43,3 +43,69 @@ for linea in a:
             id_actual = id_us
 vector = Vec(dominio, dicc)
 users[id_us] = vector
+#####################################################################
+# EJERCICIO B:
+# Tenemos que para cada peli, crear su Vector con los ratings de todos los usuarios a X pelicula
+# Dominio(ids usuarios) y la funcion user_id -> rating . Guarda los vectores en un diccionario llamado movie
+
+with open('ratings.csv', 'r') as file:
+    a = file.readlines()
+
+id_actual = 1
+lista_ids_us = []
+for linea in a:
+    linea = linea.strip('\n')
+    id_us, id_pel, ratio = linea.split(',', 2)
+    if id_us.isnumeric() == True:
+        id_us = int(id_us)
+        if id_us == id_actual:
+            if id_us not in lista_ids_us:
+                lista_ids_us.append(id_us)
+        else:
+            id_actual = id_us
+            lista_ids_us.append(id_us)
+
+dominio2 = set(lista_ids_us)
+
+id_actual = 1
+dicc = {}
+movies = {}
+
+lista_g = []
+for linea in a:
+    linea = linea.strip('\n')
+    id_us, id_pel, ratio = linea.split(',', 2)
+    if id_us.isnumeric() == True:
+        lista_g.append([int(id_us), int(id_pel), int(ratio)])
+
+def ordenar_peliculas(lista):
+    return(lista[1])
+
+lista_g.sort(key = ordenar_peliculas)
+
+pel_actual = 1
+dicc = {}
+movies = {}
+for linea in lista_g:
+    id_us = linea[0]
+    id_pel = linea[1]
+    ratio = linea[2]
+    
+    if id_pel == pel_actual:
+        dicc[id_us] = ratio
+    else:
+        vector = Vec(dominio2, dicc)
+        movies[pel_actual] = vector
+        dicc = {}
+        dicc[id_us] = ratio
+        pel_actual = id_pel
+vector = Vec(dominio2, dicc)
+movies[id_pel] = vector
+
+#####################################################################################
+# Ejercicio 3:
+
+
+
+
+
