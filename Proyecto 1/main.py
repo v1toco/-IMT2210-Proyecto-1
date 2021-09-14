f6b9d11e115be2126a03f7e7530b7b47ed7cf49c
v1toco <vborquezruiz@uc.cl>
2021-09-13 23:59:41 -0300

    Todos los ejerc listos, falta texto del .ipynb

--- Proyecto 1/main.py
+++ Proyecto 1/main.py
@@ -10,13 +10,15 @@ with open('movies.csv', 'r') as docu:
     arc = docu.readlines()
     # el len(arc) es uno mas que el ult id de pelicula
     lista_pelis = []
+    diccionario_pelis = {}
     for i in range(1, len(arc)):
         lista_pelis.append(i)
+        id_pel, nombre = arc[i].strip('\n').split(',',1)
+        id_pel = int(id_pel)
+        diccionario_pelis[id_pel] = nombre
     dominio = set(lista_pelis)
 
     # dominio para cada clase vec
-
-# soy benjita btw
 
 
 with open('ratings.csv', 'r') as file:
@@ -124,21 +126,41 @@ def vecinos(users, user_id, k):
         lista_vecinos.append(tuple(lista_sim[i]))
     return lista_vecinos
 
-a = vecinos(users, 9 , 3)
-print(a)
+#################################
+# Ejercicio 4:
+# Paso 1: crear una lista con los vectoes cercanos del user 100
+var = vecinos(users, 100, 10)
+id_pelicula = 286
+lista_vecinos_correctos = []
+suma_ptjs = 0
+for a in var:
+    id_usuario = a[0]
+    if id_pelicula in users[id_usuario].f:
+        suma_ptjs += users[id_usuario].f[id_pelicula]
+        lista_vecinos_correctos.append(id_usuario)
+promedio = suma_ptjs / len(lista_vecinos_correctos)
+# var seria los 50 vecinos cercanos al user 100
+
+######
+# ejercico 5
+cercanos = vecinos(users, 400, 30)
+
+puerta = False
+for i in cercanos:
+    user_actual = i[0]
+    if puerta == False:
+        suma = vec.scalar_mul(users[user_actual],1/30)
+        puerta = True
+    else:
+        actual = vec.scalar_mul(users[user_actual],1/30)
+        suma = vec.add(suma, actual)
+
+decreciente = sorted(suma.f.items(), key = lambda x:x[1], reverse = True)
 
 
-
-
-
-
-
-
+for a in range(10):
+    id_peli = decreciente[a][0]
+    print(diccionario_pelis[id_peli])
     
-
-
-
-
-
 
 
